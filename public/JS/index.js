d27e7e323466a933bd9d1b0248ac8f3f6c6e6810
Sir Code <taylorschneider07@yahoo.com>
2020-07-15 12:40:02 -0400

    Fixed item Sizing Issues

--- public/JS/index.js
+++ public/JS/index.js
@@ -10,15 +10,30 @@ xhttp.onreadystatechange = function () {
 		itemData.forEach((e) => {
 			var newElement = document.createElement("div");
 			newElement.setAttribute("class", "sectionData");
-			console.log(e.sprite);
 			var tempCss;
-			if (e.width) {
-				tempCss = "width:50px !important;height:auto !important;";
-			}
-			newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br><img style="${tempCss}" src="./Sprites/${e.sprite}.png" alt="" class="sectionDataSprite">`;
-			document.querySelector(".itemData").appendChild(newElement);
+			var tempWidth,
+				tempHeight;
+			let img = new Image();
+			img.onload = function () {
+				tempWidth = img.width;
+				tempHeight = img.height;
+				// if (tempWidth > tempHeight) {
+				// 	tempCss = "width:50px !important;height:auto !important;";
+				// } else if (tempHeight > tempWidth) {
+				// 	tempCss = "width:auto !important;height:50px !important;";
+				// } else if (tempHeight == tempWidth) {
+				// 	tempCss = "width:50px !important;height:50px !important;";
+				// }
+				img.setAttribute("class", "sectionDataSprite");
+				img.setAttribute("style", tempCss);
+				newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br>`;
+				document.querySelector(".itemData").appendChild(newElement);
+				document.querySelector(".itemData").appendChild(img);
+			};
+			img.src = "./Sprites/" + e.sprite + ".png";
 		});
 	}
 };
+
 xhttp.open("GET", "./Config/ItemConfig.json", true);
 xhttp.send();
