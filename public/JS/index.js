161baf33b016ad0f3ec4b91a2f137b87f9ad5126
Sir Code <taylorschneider07@yahoo.com>
2020-07-21 21:57:11 -0400

    Fixed Main Script

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,5 +1,13 @@
 var itemData;
 
+loadImage = async img => {
+    return new Promise((resolve, reject) => {
+        img.onload = async () => {
+            resolve(true);
+        };
+    });
+};
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
 	if (this.readyState == 4 && this.status == 200) {
@@ -7,20 +15,22 @@ xhttp.onreadystatechange = function () {
 		itemData = JSON.parse(obj);
 		console.log(itemData);
 
-		itemData.forEach((e) => {
+		itemData.forEach(async (e) => {
 			var newElement = document.createElement("div");
 			newElement.setAttribute("class", "sectionData");
 			var tempCss;
 			let img = new Image();
-			img.onload = function () {
-					tempCss = "width:50px !important;height:auto !important;";
-				img.setAttribute("class", "sectionDataSprite");
-				img.setAttribute("style", tempCss);
-				newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br>`;
-				document.querySelector(".itemData").appendChild(newElement);
-				document.querySelector(".itemData").appendChild(img);
-			};
 			img.src = "./Sprites/" + e.sprite + ".png";
+			await loadImage(img);
+			console.log(typeof e.dim);
+			if (typeof e.dim == Object) {
+				tempCss = `width: ${50 * e.dim[0]}px;height: ${50 * e.dim[1]};`;
+			}
+			img.setAttribute("class", "sectionDataSprite");
+			img.setAttribute("style", tempCss);
+			newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br>`;
+			document.querySelector(".itemData").appendChild(newElement);
+			document.querySelector(".itemData").appendChild(img);
 		});
 	}
 };
