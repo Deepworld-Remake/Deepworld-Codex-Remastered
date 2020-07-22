5eda3404f7631b537125baf68273db5b88a991a0
Sir Code <taylorschneider07@yahoo.com>
2020-07-21 21:10:46 -0400

    Changes

--- public/JS/index.js
+++ public/JS/index.js
@@ -7,31 +7,22 @@ xhttp.onreadystatechange = function () {
 		itemData = JSON.parse(obj);
 		console.log(itemData);
 
-		itemData.forEach((e) => {
-			var newElement = document.createElement("div");
-			newElement.setAttribute("class", "sectionData");
-			var tempCss;
-			var tempWidth,
-				tempHeight;
-			let img = new Image();
-			img.onload = function () {
-				tempWidth = img.width;
-				tempHeight = img.height;
-				// if (tempWidth > tempHeight) {
-				// 	tempCss = "width:50px !important;height:auto !important;";
-				// } else if (tempHeight > tempWidth) {
-				// 	tempCss = "width:auto !important;height:50px !important;";
-				// } else if (tempHeight == tempWidth) {
-				// 	tempCss = "width:50px !important;height:50px !important;";
-				// }
+		async (array) => {
+			for (const e of itemData) {
+				var newElement = document.createElement("div");
+				newElement.setAttribute("class", "sectionData");
+				var tempCss;
+				let img = new Image();
+				img.src = "./Sprites/" + e.sprite + ".png";
+				await img.onload();
+				tempCss = "width:50px !important;height:auto !important;";
 				img.setAttribute("class", "sectionDataSprite");
 				img.setAttribute("style", tempCss);
 				newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br>`;
 				document.querySelector(".itemData").appendChild(newElement);
 				document.querySelector(".itemData").appendChild(img);
-			};
-			img.src = "./Sprites/" + e.sprite + ".png";
-		});
+			}
+		}
 	}
 };
 
