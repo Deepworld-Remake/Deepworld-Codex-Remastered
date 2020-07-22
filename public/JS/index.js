1756a8df6c9e4da4324437e12748a47387b75218
Sir Code <taylorschneider07@yahoo.com>
2020-07-21 21:28:15 -0400

    Update index.js

--- public/JS/index.js
+++ public/JS/index.js
@@ -7,22 +7,21 @@ xhttp.onreadystatechange = function () {
 		itemData = JSON.parse(obj);
 		console.log(itemData);
 
-		async (array) => {
-			for (const e of itemData) {
-				var newElement = document.createElement("div");
-				newElement.setAttribute("class", "sectionData");
-				var tempCss;
-				let img = new Image();
-				img.src = "./Sprites/" + e.sprite + ".png";
-				await img.onload();
-				tempCss = "width:50px !important;height:auto !important;";
+		itemData.forEach((e) => {
+			var newElement = document.createElement("div");
+			newElement.setAttribute("class", "sectionData");
+			var tempCss;
+			let img = new Image();
+			img.onload = function () {
+					tempCss = "width:50px !important;height:auto !important;";
 				img.setAttribute("class", "sectionDataSprite");
 				img.setAttribute("style", tempCss);
 				newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br>`;
 				document.querySelector(".itemData").appendChild(newElement);
 				document.querySelector(".itemData").appendChild(img);
-			}
-		}
+			};
+			img.src = "./Sprites/" + e.sprite + ".png";
+		});
 	}
 };
 
