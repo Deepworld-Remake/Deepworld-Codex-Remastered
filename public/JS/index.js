467ba63b244906483d64502e691e3eac9dfed87e
Sir Code <taylorschneider07@yahoo.com>
2020-07-24 12:38:42 -0400

    Updated codex. FIxed css

--- public/JS/index.js
+++ public/JS/index.js
@@ -18,13 +18,10 @@ xhttp.onreadystatechange = function () {
 		itemData.forEach(async (e) => {
 			var newElement = document.createElement("div");
 			newElement.setAttribute("class", "sectionData");
-			var tempCss;
 			let img = new Image();
 			img.src = "./Sprites/" + e.sprite + ".png";
 			await loadImage(img);
-			tempCss = `width: ${50 * (e.dim[0] || 1)}px;height: ${50 * (e.dim[1] || 1)}px;`;
 			img.setAttribute("class", "sectionDataSprite");
-			img.setAttribute("style", tempCss);
 			newElement.innerHTML = `<span class="sectionDataName">${e.ui}</span><br><br><a href="./itemData?itemId=${e.id}" class="sectionDataLink">${e.ui}</a><br>`;
 			document.querySelector(".itemData").appendChild(newElement);
 			document.querySelector(".itemData").appendChild(img);
